18c10b9c40dc5b05489285c4961fdc615abdcb37
Rodrigo Reis <roo_reiis@hotmail.com>
2024-09-22 01:25:06 -0300

    feat: cONFIGURAÇÃO DO DOCKER PARA O FRONTENDT

---
+++ environments.prod.ts
@@ -0,0 +1,4 @@
+export const environments = {
+  production: true,
+  BACKEND_URL: 'http://localhost:3333'
+}
---
+++ environments.ts
@@ -0,0 +1,4 @@
+export const environments = {
+  production: false,
+  BACKEND_URL: 'http://localhost:3333'
+}
